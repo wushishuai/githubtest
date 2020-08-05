e481c4adcac89e6cff0c4cde2f72ecad9785f138
wushishuai <15651872368@163.com>
2020-08-05 10:21:11 +0800

    recyclerviewtestdebug

--- RecyclerViewTest2/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
+++ RecyclerViewTest2/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
@@ -1,4 +1,70 @@
 package com.example.recyclerviewtest;
 
-public class FruitAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private List<Fruit> mFruitList;
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        View fruitView;
+        ImageView fruitImage;
+        TextView fruiName;
+        public ViewHolder(View view){
+            super(view);
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruiName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+    public FruitAdapter(List<Fruit> fruitList){
+        mFruitList = fruitList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAbsoluteAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(view.getContext(),"you clicked view "+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.fruitImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAbsoluteAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(view.getContext(),"you clicked image"+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruiName.setText(fruit.getName());
+
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
 }
