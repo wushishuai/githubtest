20e30e3416805ec7591ddf3ab5ab04bf2cbd4af2
wushishuai <15651872368@163.com>
2020-08-05 15:58:45 +0800

    fragmenttest

---
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.fragmenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                switch (view.getId()) {
+//                    case R.id.button:
+//                        replaceFragment(new AnotherRightFragment());
+//                        break;
+//                    default:
+//                        break;
+//
+//                }
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
